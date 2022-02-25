c45c51ae44ced232c9988136071eb6c6fc0a0e0d
Ndaru <muhamad.ndaru@gmail.com>
2022-02-25 17:43:41 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Home | Movie App')
+
+@section('content')
+    <div class="container movie-wrapper">
+        <div class="row">
+            <div class="col-md-3 movie-item">
+                <div class="col-md-12 movie-content">
+                    <h1>Spiderman 3</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero eum molestias reprehenderit,
+                        necessitatibus dolorem possimus ut quia obcaecati</p>
+                    <span class="badge bg-warning">Horror</span>
+                    <span>Tahun Terbit: 2021</span>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#"><i class="uil uil-film me-1"></i>MovieApp</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="uil uil-estate me-1"></i>Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="uil uil-youtube me-1"></i>Movie</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
